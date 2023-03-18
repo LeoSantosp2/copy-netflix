13a28b86fee26a975b4d9ebbc0abc016f0f88463
Leonardo Santos <leogilberto2010@hotmail.com>
2023-03-18 20:56:45 -0300

    Adicionando mais cores

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,3 +1,6 @@
 export const netflixRed = '#E50914';
+export const netflixDarkRed = '#B20710';
 export const black = '#000000';
 export const white = '#F2F2F2';
+
+export const shadow = 'rgba(0, 0, 0, 0.5)';
